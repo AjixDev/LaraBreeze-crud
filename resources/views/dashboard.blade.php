e74e40a67ae01b21b24469b78ad605a150c42fea
Roy Aji <ajixdev@gmail.com>
2023-07-17 12:16:47 +0300

    CSS update - width, borders, padding

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,31 +6,33 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 grid grid-cols-1 lg:grid-cols-2 gap-8">
+        <div class="max-w-8xl mx-auto sm:px-6 lg:px-8 grid grid-cols-1 lg:grid-cols-2 gap-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <p class="logged-in p-6">You're logged in</p>
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <div class="grid grid-cols-1">
-                        <p class="block text-gray-700 font-bold mb-2 bg-gray-100">Add New Country</p>
+                <div class="p-6 bg-white">
+                    <div class="grid grid-cols-1 border border-gray-200 divide-y divide-slate-200">
+                        <p class="block text-gray-700 p-6 font-bold bg-gray-100">Add New Country</p>
                         <!-- Create State Form -->
                         <form action="{{ route('states.store') }}" method="POST">
                             @csrf
-                            <div class="mb-4">
+                            <div class="mt-4 px-6">
                                 <label for="state-name-input" class="block text-gray-700 font-bold mb-2">Name</label>
-                                <input type="text" id="state-name-input" name="name" required class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-200">
+                                <input type="text" id="state-name-input" name="name" required class="w-full py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-200">
                             </div>
-                            <div class="mb-4">
+                            <div class="mt-4 px-6">
                                 <label for="state-iso-input" class="block text-gray-700 font-bold mb-2">ISO</label>
-                                <input type="text" id="state-iso-input" name="iso" required class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-200">
+                                <input type="text" id="state-iso-input" name="iso" required class="w-full py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-200">
                             </div>
-                            <button type="submit" class="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">Create State</button>
+                            <div class="p-6 w-1 flex items-end">
+                                <button type="submit" class="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">Create State</button>
+                            </div>
                         </form>
                     </div>
                 </div>
             </div>
 
             <!-- List of States -->
-            <div class="p-6 bg-white border-b border-gray-200">
+            <div class="p-6 bg-white divide-y divide-slate-200">
                 <table class="table-auto w-full border-separate border-spacing-2 border border-slate-400">
                     <thead class="border-separate border-spacing-2 border border-slate-400">
                         <tr>
